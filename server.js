65cf23cff177defa756cecbfb721a88e4f7db3aa
Jojo Zhang <jojozhang0327@gmail.com>
2019-10-16 21:36:17 -0400

    complete MVP

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const logger = require("./middleware/logger.js");
+
+const authRouter = require("./auth/auth-router.js");
+const usersRouter = require("./users/user-router.js");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+server.use(logger);
+
+server.use("/api", authRouter);
+server.use("/api/users", usersRouter);
+
+server.get("/", (req, res) => {
+  res.send("Server is working!");
+});
+
+module.exports = server;
